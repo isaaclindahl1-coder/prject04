68a322c8b2e33b16cc4d575329f9f0f3c0ab4596
Isaac Lindahl <cmp_islindahl@ects.org>
2026-09-10 09:28:22 -0400

    practice5

---
+++ practice5.py
@@ -0,0 +1,4 @@
+phrase = input("Enter a phrase: ").strip()
+print(f"the first 3 characters are: {phrase[0:3]}")
+print(f"the last 3 characters are: {phrase[-3::]}")
+print(f"every second character is: {phrase[0::2]}")
